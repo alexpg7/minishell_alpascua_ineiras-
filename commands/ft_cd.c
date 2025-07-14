b56fd601a7ac7e3675960e5e52d274f1f1de0db5
Ismael Neiras Garcia <ineiras-@cbr12s1.42barcelona.com>
2025-07-14 19:44:02 +0200

    cd update

--- commands/ft_cd.c
+++ commands/ft_cd.c
@@ -14,6 +14,45 @@ static void change_old_pwd(t_vars *vars, char *old_pwd)
 		env->content = ft_strjoin("OLDPWD=", old_pwd + 4);
 }
 
+static void change_pwd(t_vars *vars, char *new_pwd)
+{
+	t_list	*env;
+
+	env = vars->env;
+	while (env && ft_strncmp("PWD=", env->content, 4) != 0)
+		env = env->next;
+	/*
+	if (ft_strncmp("HOME=", old_pwd, 5) == 0)
+		env->content = ft_strjoin("OLDPWD=", old_pwd + 5);*/
+	if (ft_strncmp("OLDPWD=", new_pwd, 7) == 0)
+		env->content = ft_strjoin("PWD=", new_pwd + 7);
+}
+/*
+static void cd_move(t_vars *vars, char *dir_name)
+{
+
+}*/
+
+static void swap_pwd(t_vars *vars)
+{
+	t_list	*act_env;
+	t_list	*old_env;
+	char	*content;
+	char	*content_2;
+
+	act_env = vars->env;
+	old_env = vars->env;
+
+	while (act_env && ft_strncmp("PWD=", act_env->content, 4) != 0)
+		act_env = act_env->next;
+	content = act_env->content;
+	while (old_env && ft_strncmp("OLDPWD=", old_env->content, 7) != 0)
+		old_env = old_env->next;
+	content_2 = old_env->content;
+	change_old_pwd(vars, content);
+	change_pwd(vars, content_2);
+}
+
 static void cd_double_point(t_vars *vars)
 {
 	t_list *env;
@@ -50,7 +89,6 @@ static void	cd_not_args(t_vars *vars)
 	env = vars->env;
 	while (env && ft_strncmp(env->content, "PWD=", 4) != 0)
 		env = env->next;
-	//ft_printf("%s", env->content);
 	change_old_pwd(vars, env->content);
 	env->content = content;
 }
@@ -63,28 +101,23 @@ void ft_cd(t_vars *vars, char **argv)
 	argc = count_args(argv);
 	if (argc == 1 || (argc == 2 && ft_strcmp(argv[1], "~") == 0))
 		cd_not_args(vars);
-	else if ((argc == 2) && (ft_strcmp(argv[1], "/") == 0))
-		slash_cd(vars);
-	else if ((argc == 2) && (ft_strcmp(argv[1], ".") == 0))
-		;
-	else if ((argc == 2) && (ft_strcmp(argv[1], "..") == 0))
-		cd_double_point(vars);
-	else if (argc == 2 && (directory = opendir(ft_strjoin3(ft_pwd(vars), "/", argv[1]))) != NULL) //protect
-		//cd_path();
-		ft_printf("yes\n");
-	else
-		ft_printf("cd: not such file or directory: %s\n", argv[1]);
-	/*
-	if ((argc == 2) && (ft_strcmp(argv[1], "/") == 0))
-	cd_not_args(vars, argv);
-	if ((argc == 2) && (ft_strcmp(argv[1], "~") == 0))
-	cd_not_args(vars, argv);
-	if ((argc == 2) && (ft_strcmp(argv[1], ".") == 0))git
-	cd_not_args(vars, argv);
-	if ((argc == 2) && (ft_strcmp(argv[1], "..") == 0))
-	cd_not_args(vars, argv);
+	else if (argc == 2)
 	{
-	cd_not_args(vars, argv);
-	*/
-
+		directory = opendir(ft_strjoin3(ft_pwd(vars), "/", argv[1]));
+		if (ft_strcmp(argv[1], "/") == 0)
+			slash_cd(vars);
+		if (ft_strcmp(argv[1], "-") == 0)
+			swap_pwd(vars);
+		else if (ft_strcmp(argv[1], ".") == 0)
+			;
+		else if(ft_strcmp(argv[1], "..") == 0)
+			cd_double_point(vars);
+		else if (directory != NULL)
+		{
+			//cd_move_dir("yes\n");
+			closedir(directory);
+		}
+		else
+			ft_printf("cd: not such file or directory: %s\n", argv[1]);
+	}
 }
