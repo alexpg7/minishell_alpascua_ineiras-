2aba67bc7f8f3e1d9d3ed224e5676e51d945adf4
Mel Neiras <melneirasg@uoc.edu>
2025-07-15 12:27:35 +0200

    up

--- commands/ft_cd.c
+++ commands/ft_cd.c
@@ -1,56 +1,29 @@
 
 #include "../src/minishell.h"
 
-static void change_old_pwd(t_vars *vars, char *old_pwd)
+static int	cd_move_dir(t_vars *vars, char **argv)
 {
+	DIR		*directory;
 	t_list	*env;
+	char	*path;
 
+	path = ft_strjoin3(ft_pwd(vars), "/", argv[1]);
+	if (!path)
+		return (-1);
+	directory = opendir(path);
+	if (directory == NULL)
+	{
+		free(path);
+		return (-1);
+	}
 	env = vars->env;
-	while (env && ft_strncmp("OLDPWD=", env->content, 7) != 0)
+	while (env && ft_strncmp(env->content, "PWD=", 4) != 0)
 		env = env->next;
-	if (ft_strncmp("HOME=", old_pwd, 5) == 0)
-		env->content = ft_strjoin("OLDPWD=", old_pwd + 5);
-	if (ft_strncmp("PWD=", old_pwd, 4) == 0)
-		env->content = ft_strjoin("OLDPWD=", old_pwd + 4);
-}
-
-static void change_pwd(t_vars *vars, char *new_pwd)
-{
-	t_list	*env;
-
-	env = vars->env;
-	while (env && ft_strncmp("PWD=", env->content, 4) != 0)
-		env = env->next;
-	/*
-	if (ft_strncmp("HOME=", old_pwd, 5) == 0)
-		env->content = ft_strjoin("OLDPWD=", old_pwd + 5);*/
-	if (ft_strncmp("OLDPWD=", new_pwd, 7) == 0)
-		env->content = ft_strjoin("PWD=", new_pwd + 7);
-}
-/*
-static void cd_move(t_vars *vars, char *dir_name)
-{
-
-}*/
-
-static void swap_pwd(t_vars *vars)
-{
-	t_list	*act_env;
-	t_list	*old_env;
-	char	*content;
-	char	*content_2;
-
-	act_env = vars->env;
-	old_env = vars->env;
-
-	while (act_env && ft_strncmp("PWD=", act_env->content, 4) != 0)
-		act_env = act_env->next;
-	content = act_env->content;
-	while (old_env && ft_strncmp("OLDPWD=", old_env->content, 7) != 0)
-		old_env = old_env->next;
-	content_2 = old_env->content;
-	change_old_pwd(vars, content);
-	change_pwd(vars, content_2);
+	change_old_pwd(vars, env->content);
+	env->content = ft_strjoin("PWD=", path);
+	chdir(path);
+	closedir(directory);
+	return (0);
 }
 
 static void cd_double_point(t_vars *vars)
@@ -64,6 +37,7 @@ static void cd_double_point(t_vars *vars)
 	content = minus_dir(env->content);
 	change_old_pwd(vars, env->content);
 	env->content = content;
+	chdir(env->content + 4);
 }
 
 static void	slash_cd(t_vars *vars)
@@ -75,6 +49,7 @@ static void	slash_cd(t_vars *vars)
 		env = env->next;
 	change_old_pwd(vars, env->content);
 	env->content = ft_strjoin("PWD=", "/");
+	chdir(env->content + 4);
 }
 
 static void	cd_not_args(t_vars *vars)
@@ -91,33 +66,27 @@ static void	cd_not_args(t_vars *vars)
 		env = env->next;
 	change_old_pwd(vars, env->content);
 	env->content = content;
+	chdir(env->content + 4);
 }
 
 void ft_cd(t_vars *vars, char **argv)
 {
 	int argc;
-	DIR *directory;
 
 	argc = count_args(argv);
 	if (argc == 1 || (argc == 2 && ft_strcmp(argv[1], "~") == 0))
 		cd_not_args(vars);
 	else if (argc == 2)
 	{
-		directory = opendir(ft_strjoin3(ft_pwd(vars), "/", argv[1]));
 		if (ft_strcmp(argv[1], "/") == 0)
 			slash_cd(vars);
-		if (ft_strcmp(argv[1], "-") == 0)
+		else if (ft_strcmp(argv[1], "-") == 0)
 			swap_pwd(vars);
 		else if (ft_strcmp(argv[1], ".") == 0)
 			;
 		else if(ft_strcmp(argv[1], "..") == 0)
 			cd_double_point(vars);
-		else if (directory != NULL)
-		{
-			//cd_move_dir("yes\n");
-			closedir(directory);
-		}
-		else
+		else if (cd_move_dir(vars, argv) == -1)
 			ft_printf("cd: not such file or directory: %s\n", argv[1]);
 	}
 }
---
+++ commands/ft_cd2.c
@@ -0,0 +1,51 @@
+
+
+#include "../src/minishell.h"
+
+void change_old_pwd(t_vars *vars, char *old_pwd) // Changes $OLD_PWD for current PWW;
+{
+	t_list	*env;
+
+	env = vars->env;
+	while (env && ft_strncmp("OLDPWD=", env->content, 7) != 0)
+		env = env->next;
+	if (ft_strncmp("HOME=", old_pwd, 5) == 0)
+		env->content = ft_strjoin("OLDPWD=", old_pwd + 5);
+	if (ft_strncmp("PWD=", old_pwd, 4) == 0)
+		env->content = ft_strjoin("OLDPWD=", old_pwd + 4);
+}
+
+void change_pwd(t_vars *vars, char *pwd) // Changes $PWD for $OLD_PWD;
+{
+	t_list	*env;
+
+	env = vars->env;
+	while (env && ft_strncmp("PWD=", env->content, 4) != 0)
+		env = env->next;
+	/*
+	if (ft_strncmp("HOME=", old_pwd, 5) == 0)
+		env->content = ft_strjoin("OLDPWD=", old_pwd + 5);*/
+	if (ft_strncmp("OLDPWD=", pwd, 7) == 0)
+		env->content = ft_strjoin("PWD=", pwd + 7);
+}
+
+void swap_pwd(t_vars *vars)
+{
+	t_list	*act_env;
+	t_list	*old_env;
+	char	*content;
+	char	*content_2;
+
+	act_env = vars->env;
+	old_env = vars->env;
+
+	while (act_env && ft_strncmp("PWD=", act_env->content, 4) != 0)
+		act_env = act_env->next;
+	content = act_env->content;
+	while (old_env && ft_strncmp("OLDPWD=", old_env->content, 7) != 0)
+		old_env = old_env->next;
+	content_2 = old_env->content;
+	change_old_pwd(vars, content);
+	change_pwd(vars, content_2);
+	chdir(content_2 + 4);
+}
