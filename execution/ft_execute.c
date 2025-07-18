90d2efd6f86dc12c8a7b647c0d5f2ffa15205fca
Alex Pascual Galera <alpascua@cbr11s2.42barcelona.com>
2025-07-18 14:09:06 +0200

    fixed the bug on out redirection with builtin commands

--- execution/ft_execute.c
+++ execution/ft_execute.c
@@ -1,30 +1,23 @@
 #include "../src/minishell.h"
 
-int	ft_builtin(char **comm, t_vars *vars)
+int	ft_checkfd(char *file, int fd, int final)
 {
-	char *pwd;
-
-	if (ft_strcmp("echo", comm[0]) == 0)
-		ft_echo(comm + 1);
-	else if (ft_strcmp("cd", comm[0]) == 0)
-		ft_cd(vars, comm);
-	else if (ft_strcmp("pwd", comm[0]) == 0)
+	if (fd == -1)
 	{
-		pwd = ft_pwd(comm + 1, vars);
-		if (pwd)
-			ft_printf("%s\n", pwd);
+		perror(file);
+		return (-1);
 	}
-	else if (ft_strcmp("export", comm[0]) == 0)
-		ft_export(vars, comm[1]); //change to accept different arguments (+ not an identifier: if not valid var)
-	else if (ft_strcmp("unset", comm[0]) == 0)
-		ft_printf("\"executing unset...\"\n");
-	else if (ft_strcmp("env", comm[0]) == 0)
-		ft_env(vars);
-	else if (ft_strcmp("exit", comm[0]) == 0)
-		ft_exit(NULL, 0, vars);
-	else
-		return (0);
-	return (1);
+	if (dup2(fd, final) == -1)
+	{
+		perror("dup2");
+		return (-1);
+	}
+	if (close(fd) == -1)
+	{
+		perror("close");
+		return (-1);
+	}
+	return (0);
 }
 
 int	ft_heredoc(char *lim)
@@ -38,22 +31,7 @@ int	ft_readin2(char *file)
 	int	fd;
 
 	fd = open(file, O_RDONLY);
-	if (fd == -1)
-	{
-		perror(file);
-		return (-1);
-	}
-	if (dup2(fd, 0) == -1)
-	{
-		perror("dup2");
-		return (-1);
-	}
-	if (close(fd) == -1)
-	{
-		perror("close");
-		return (-1);
-	}
-	return (0);
+	return (ft_checkfd(file, fd, 0));
 }
 
 int	ft_readin(char *file, int mode)
@@ -74,36 +52,57 @@ int	ft_readout(char *file, int mode)
 		fd = open(file, O_WRONLY | O_APPEND | O_CREAT, 0644);
 	else
 		fd = open(file, O_TRUNC | O_WRONLY | O_CREAT, 0644);
-	if (fd == -1)
-	{
-		perror(file);
-		return (-1);
-	}
-	if (dup2(fd, 1) == -1)
-	{
-		perror("dup2");
-		return (-1);
-	}
-	if (close(fd) == -1)
-	{
-		perror("close");
-		return (-1);
-	}
+	return (ft_checkfd(file, fd, 1));
 	return (0);
 }
 
-void	ft_set_redir(t_command *command, t_vars *vars)
+void	ft_set_redir(t_command *command, t_vars *vars, int mode)
 {
-	if (command->infile)
+	if (command->infile && (mode == 0 || mode == 2))
 	{
 		if (ft_readin(command->infile, command->hd) == -1)
 			ft_exit(NULL, 1, vars);
 	}
-	if (command->outfile)
+	if (command->outfile && (mode == 1 || mode == 2))
 	{
 		if (ft_readout(command->outfile, command->ap) == -1)
 			ft_exit(NULL, 1, vars);
 	}
+}
+
+int	ft_builtin2(t_command *com, t_vars *vars)
+{
+	char *pwd;
+
+	ft_set_redir(com, vars, 2);
+	if (ft_strcmp("echo", com->comm[0]) == 0)
+		ft_echo(com->comm + 1);
+	else if (ft_strcmp("pwd", com->comm[0]) == 0)
+	{
+		pwd = ft_pwd(com->comm + 1, vars);
+		if (pwd)
+			ft_printf("%s\n", pwd);
+	}
+	else if (ft_strcmp("env", com->comm[0]) == 0)
+		ft_env(vars);
+	else
+		return (0);
+	return (1);
+}
+
+int	ft_builtin1(t_command *com, t_vars *vars)
+{
+	if (ft_strcmp("cd", com->comm[0]) == 0)
+		ft_cd(vars, com->comm);
+	else if (ft_strcmp("export", com->comm[0]) == 0)
+		ft_export(vars, com->comm[1]); //change to accept different arguments (+ not an identifier: if not valid var)
+	else if (ft_strcmp("unset", com->comm[0]) == 0)
+		ft_printf("\"executing unset...\"\n");
+	else if (ft_strcmp("exit", com->comm[0]) == 0)
+		ft_exit(NULL, 0, vars);
+	else
+		return (0);
+	return (1);
 }
 
 void	ft_exec1(t_command *command, t_vars *vars)
@@ -112,20 +111,24 @@ void	ft_exec1(t_command *command, t_vars *vars)
 	char	**envp;
 	char	*path;
 
-	if (!ft_builtin(command->comm, vars))
+	if (!ft_builtin1(command, vars))
 	{
 		pid = fork();
 		if (pid == 1)
 			perror("fork");
 		else if (pid == 0)
 		{
-			ft_set_redir(command, vars);
-			envp = ft_getenv(vars->env); //protect
-			path = ft_strjoin("/usr/bin/", command->comm[0]);//protect and SEARCH FOR TRUE PATH
-			if (!envp || !path)
-				ft_exit(NULL, 1, vars);
-			if (execve(path, command->comm, envp) == -1) //save exit status
-				ft_exit(NULL, 1, vars); //SHOULD FREE ENVP AND PATH
+			if (!ft_builtin2(command, vars))
+			{
+				ft_set_redir(command, vars, 2);
+				envp = ft_getenv(vars->env); //protect
+				path = ft_strjoin("/usr/bin/", command->comm[0]);//protect and SEARCH FOR TRUE PATH
+				if (!envp || !path)
+					ft_exit(NULL, 1, vars);
+				if (execve(path, command->comm, envp) == -1) //save exit status
+					ft_exit(NULL, 1, vars); //SHOULD FREE ENVP AND PATH
+			}
+			ft_exit(NULL, 0, vars);
 		}
 		else
 			waitpid(pid, NULL, 0);//save exit status somewhere
