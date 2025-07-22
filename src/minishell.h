34d6b47250bd8888e9f2969f6dc99ff2734bccaa
Mel Neiras <melneirasg@uoc.edu>
2025-07-22 21:25:50 +0200

    up

--- src/minishell.h
+++ src/minishell.h
@@ -50,9 +50,9 @@ char	*ft_searchdollar(char *str, t_vars *vars);
 char	*ft_removequotes(char *str);
 
 // COMMANDS
-
 void	ft_exit(char *input, int ret, t_vars *vars);
 char	*ft_pwd(char **comm, t_vars *vars);
+char 	*ft_new_pwd(void);
 char	*ft_path(t_vars *vars);
 void	ft_env(t_vars *vars);
 void	ft_export(t_vars *vars, char *arg);
