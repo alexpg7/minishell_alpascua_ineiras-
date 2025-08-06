603cda5de80da379243dae0122b988bba725dde3
Ismael Neiras Garcia <ineiras-@cbr14s3.42barcelona.com>
2025-08-06 15:57:08 +0200

    protect

--- commands/ft_export.c
+++ commands/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ineiras- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ineiras- <ineiras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 18:28:03 by ineiras-          #+#    #+#             */
-/*   Updated: 2025/08/04 18:29:57 by ineiras-         ###   ########.fr       */
+/*   Updated: 2025/08/06 15:52:37 by ineiras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,7 @@ void	ft_export(t_vars *vars, char **arg, int mode)
 		if (ft_inenv(*arg, vars))
 			ft_subsenv(*arg, vars);
 		else
-			ft_lstadd_back(&vars->env, ft_lstnew(new_var(vars, *arg)));//protect
+			ft_lstadd_back(&vars->env, ft_lstnew(new_var(vars, *arg)));
 		if (mode == 1)
 			break ;
 		arg++;
@@ -92,14 +92,19 @@ char	*new_var(t_vars *vars, char *arg)
 		return (NULL);
 	str = ft_strdup_eq(arg);
 	if (!str)
-		ft_exit(NULL, 1, vars);
+		ft_exit(NULL, 2, vars);
 	str_ae = ft_strdup(ft_strchr(arg, '=') + 1);
 	if (!str_ae)
-		ft_exit(NULL, 1, vars);
-	str_ae = ft_removequotes(ft_searchdollar(str_ae, vars));//protect
+		ft_exit(NULL, 2, vars);
+	str_ae = ft_searchdollar(str_ae, vars);
 	if (!str_ae)
-		ft_exit(NULL, 1, vars);
+		ft_exit(NULL, 2, vars);
+	str_ae = ft_removequotes(str_ae);
+	if (!str_ae)
+		ft_exit(NULL, 2, vars);
 	str_en = ft_strjoin(str, str_ae);
+	if (!str_en)
+		ft_exit(NULL, 2, vars);
 	ft_free_both(str, str_ae);
 	return (str_en);
 }
