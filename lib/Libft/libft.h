dd704c75faea416085e7d5e3d6a2bfc513284c4c
Alex Pascual Galera <alpascua@cbr13s1.42barcelona.com>
2025-08-11 13:17:44 +0200

    ft_isspace added to Libft

--- lib/Libft/libft.h
+++ lib/Libft/libft.h
@@ -44,6 +44,8 @@ int		ft_isdigit(int c);
 
 int		ft_isprint(int c);
 
+int		ft_isspace(char c);
+
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
