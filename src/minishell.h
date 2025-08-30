c0c91a8a6564de5db09708dc3d230e0ce195e24a
Ismael Neiras Garcia <ineiras-@cbr11s2.42barcelona.com>
2025-08-30 17:45:44 +0200

    Norm minishell.h

--- src/minishell.h
+++ src/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ineiras- <ineiras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 19:56:17 by alpascua          #+#    #+#             */
-/*   Updated: 2025/08/30 17:21:58 by ineiras-         ###   ########.fr       */
+/*   Updated: 2025/08/30 17:44:54 by ineiras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,22 +89,13 @@ t_lst		*ft_lstnew_lst(void *content, void **content_p);
 void		ft_lstadd_back_lst(t_lst **lst, t_lst *new);
 void		ft_lstdelone_lst(t_lst *lst);
 void		ft_lstclear_lst(t_lst **lst);
-void		ft_freecommand(t_command *command);////////////ERASE
 void		ft_freeinput(t_input ***input, int np, t_vars *vars);
 void		ft_free_lst(void **ptr);
 void		ft_free_both(char *str1, char *str2);
-//void	ft_free_three(char *str1, char *str2, char *str3);
-
 // SIGNALS
 void		ft_signal(int code);
 void		ft_sigint_heredoc(int sig);
 void		ft_sigquit_heredoc(int sig);
-
-// SPLITMINI ///////////////////////////////////////////////
-char		**ft_splitmini(char const *s, char c, t_vars *vars);
-void		ft_jumpcom(char const **s, char sep);
-int			ft_special(char c);
-void		ft_nextword(const char **s, char sep);
 
 // SPLITMINI2
 char		**ft_splitmini2(char *str, t_vars *vars);
@@ -134,19 +125,10 @@ int			ft_countquotes(char *comm);
 char		*ft_searchvar(char *comm, int len, t_vars *vars);
 void		ft_copyvar(char **dest, char *src, int *i, t_vars *vars);
 
-// SEARCH DOLLAR /////////////////////////////////////////
-char		*ft_searchdollar(char *str, t_vars *vars);
-char		*ft_removequotes(char *str);
-int			ft_varlen(char *str);
-int			ft_isvar(char *str, t_vars *vars, int mode);
-int			ft_countvars(char *str, t_vars *vars);
-int			ft_valuelen(char *str, t_vars *vars);
-
 // COMMANDS
 void		ft_exit(char *input, int ret, t_vars *vars);
 char		*ft_pwd(char **comm, t_vars *vars);
 char		*ft_new_pwd(void);
-char		*ft_path(t_vars *vars);
 void		ft_env(t_vars *vars);
 void		ft_export(t_vars *vars, char **arg, int mode);
 void		ft_echo(char **args);
@@ -154,20 +136,18 @@ void		ft_cd(t_vars *vars, char **argv);
 char		**ft_getenv(t_list *env);
 void		ft_unset(char **args, t_vars *vars);
 int			ft_strisalnum2(char *str);
-char		*new_var(t_vars *vars, char *arg);/////
 
 // New CD COMMANDS
-void	ft_cd(t_vars *vars, char **argv);
-void	ft_cd_not_args(t_vars *vars);
-void	ft_cd_two_arg(t_vars *vars, char **argv);
-int		ft_cd_move_dir(t_vars *vars, char **argv, char *path);
-void	ft_swap_pwd(t_vars *vars);
-void	ft_cd_double_point(t_vars *vars);
-
+void		ft_cd(t_vars *vars, char **argv);
+void		ft_cd_not_args(t_vars *vars);
+void		ft_cd_two_arg(t_vars *vars, char **argv);
+int			ft_cd_move_dir(t_vars *vars, char **argv, char *path);
+void		ft_swap_pwd(t_vars *vars);
+void		ft_cd_double_point(t_vars *vars);
 
 // New CD UTILS
 char		*ft_getenv_var(t_vars *vars, char *var_name);
-int  		ft_setenv_var(t_vars *vars, char *pwd, char *var_name);
+int			ft_setenv_var(t_vars *vars, char *pwd, char *var_name);
 void		ft_path_fail(t_vars *vars, char *pwd, char *error_m);
 void		ft_create_var(t_vars *vars, char *pwd, char *var_name);
 int			count_args(char **argv);
@@ -177,16 +157,8 @@ int			ft_free_minus_one(char *str);
 void		ft_cd_point(t_vars *vars);
 void		ft_set_both(t_vars *vars, char *pwd);
 
-
 // PROGRAMS
 void		ft_init(t_vars *vars, char **envp);
-
-// EXECUTION ///////////////////////////////////////////////////
-//int			ft_heredoc(char *lim, int tag);
-char		*ft_findpath(char *comm, char **envp, t_vars *vars);
-int			exitstatus2(int status);
-void		ft_execmore(t_command *command, t_vars *vars);
-//void		ft_waitall(t_input **input, int len, t_vars *vars);
 
 // EXIT STATUS
 int			exitstatus2(int status);
@@ -200,14 +172,6 @@ int			ft_execbetween(t_command *comm, int *pid, int **pip, t_vars *vars);
 
 // EXEC LAST
 int			ft_execlast(t_command *comm, int *pid, int **pip, t_vars *vars);
-
-// EXECUTION UTILS //////////////////////////////////////////
-int			ft_searchbuiltin(t_command *com);
-//int			**ft_freepip(int **arr, int len, int index);
-
-// CREATE COMMAND (struct command) /////////////////////////////////////////////
-t_command	*ft_createcomm(char **comm, t_vars *vars);
-int			ft_commandlen(char **comm);
 
 // NEW EXECUTION
 void		ft_execute2(t_input **input, t_vars *vars);
@@ -240,7 +204,5 @@ int			ft_search_tokken_2(t_input *input, char tokken, int *pos);
 void		ft_read_in(t_input *input, t_vars *vars, int pos);
 void		ft_write_out(t_input *input, t_vars *vars, int pos);
 void		ft_read_app(t_input *input, t_vars *vars, int pos);
-
-// EXECUTION WITH PIPES
 
 #endif
