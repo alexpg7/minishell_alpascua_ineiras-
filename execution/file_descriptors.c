435260069ed5a11cceff6ea63e88896df9f6a227
Alex Pascual Galera <alpascua@cbr12s1.42barcelona.com>
2025-08-30 20:18:31 +0200

    file existance protection

--- execution/file_descriptors.c
+++ execution/file_descriptors.c
@@ -12,15 +12,24 @@
 
 #include "../src/minishell.h"
 
+static void	ft_message2(char *s1, char *path, char *s2)
+{
+	ft_putstr_fd(s1, 2);
+	ft_putstr_fd(path, 2);
+	ft_putstr_fd(s2, 2);
+}
+
 void	ft_write_out(t_input *input, t_vars *vars, int pos)
 {
 	int	mask;
 
-	if (access(input->word[pos], W_OK) != 0)
+	if (access(input->word[pos], F_OK) == 0)
 	{
-		ft_putstr_fd("cannot write \"", 2);
-		ft_putstr_fd(input->word[pos], 2);
-		ft_putstr_fd("\": permission denied\n", 2);
+		if (access(input->word[pos], W_OK) != 0)
+		{
+			ft_message2("cannot write \"", input->word[pos], "");
+			ft_putstr_fd("\": permission denied\n", 2);
+		}
 	}
 	mask = O_CREAT | O_TRUNC | O_WRONLY;
 	input->last_out = open(input->word[pos], mask, 0644);
@@ -41,11 +50,13 @@ void	ft_read_app(t_input *input, t_vars *vars, int pos)
 {
 	int	mask;
 
-	if (access(input->word[pos], W_OK) != 0)
+	if (access(input->word[pos], F_OK) == 0)
 	{
-		ft_putstr_fd("cannot write \"", 2);
-		ft_putstr_fd(input->word[pos], 2);
-		ft_putstr_fd("\": permission denied\n", 2);
+		if (access(input->word[pos], W_OK) != 0)
+		{
+			ft_message2("cannot write \"", input->word[pos], "");
+			ft_putstr_fd("\": permission denied\n", 2);
+		}
 	}
 	mask = O_CREAT | O_WRONLY | O_APPEND;
 	input->last_out = open(input->word[pos], mask, 0644);
@@ -64,18 +75,22 @@ void	ft_read_app(t_input *input, t_vars *vars, int pos)
 
 void	ft_read_in(t_input *input, t_vars *vars, int pos)
 {
-	if (access(input->word[pos], W_OK) != 0)
+	if (access(input->word[pos], F_OK) == 0)
 	{
-		ft_putstr_fd("cannot read \"", 2);
-		ft_putstr_fd(input->word[pos], 2);
-		ft_putstr_fd("\": permission denied\n", 2);
+		if (access(input->word[pos], R_OK) != 0)
+		{
+			ft_message2("cannot read \"", input->word[pos], "");
+			ft_putstr_fd("\": permission denied\n", 2);
+		}
 	}
+	else
+		ft_message2(input->word[pos], ": no such file or directory\n", "");
 	input->last_in = open(input->word[pos], O_RDONLY);
 	if (input->last_in == -1)
 		ft_exit(NULL, 1, vars);
 	if (input->last_in <= 0)
 		return ;
-	if (dup2(input->last_in, STDOUT_FILENO) == -1)
+	if (dup2(input->last_in, STDIN_FILENO) == -1)
 	{
 		close(input->last_in);
 		ft_exit(NULL, 1, vars);
