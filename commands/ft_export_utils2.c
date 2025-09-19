31fac3138389d7bcd94abb3e4bca4d01d57b24b1
Alex Pascual Galera <alpascua@cbr13s5.42barcelona.com>
2025-09-19 16:36:38 +0200

    fixed the export print

--- commands/ft_export_utils2.c
+++ commands/ft_export_utils2.c
@@ -2,18 +2,26 @@
 
 void	ft_printexport(char *str)
 {
-	int i;
+	int	i;
+	int	flag;
 
 	i = 0;
+	flag = 0;
+	ft_putstr_fd("declare -x ", 1);
 	while(str[i])
 	{
-		ft_putchar_fd(str[i], 1);
 		if (str[i] == '=')
-			ft_putchar_fd('\"', 1);
-		if (str[i] == '\"')
-			ft_putchar_fd('/', 1);
+		{
+			flag = 1;
+			ft_putstr_fd("=\"", 1);
+		}
+		else if (str[i] == '\"')
+			ft_putstr_fd("\\\"", 1);
+		else
+			ft_putchar_fd(str[i], 1);
 		i++;
 	}
-	ft_putchar_fd('\"', 1);
+	if (flag == 1)
+		ft_putchar_fd('\"', 1);
 	ft_putchar_fd('\n', 1);
 }
